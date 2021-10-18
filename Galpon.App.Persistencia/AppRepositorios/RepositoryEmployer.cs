dc5574044a60d68e0fb3455f6468c61e405769f9
andresR <anrioco2000@gmail.com>
2021-10-18 14:24:00 -0500

    solucion capa persistencia

--- Galpon.App.Persistencia/AppRepositorios/RepositoryEmployer.cs
+++ Galpon.App.Persistencia/AppRepositorios/RepositoryEmployer.cs
@@ -4,29 +4,9 @@ using Galpon.App.Dominio;
 
 namespace Galpon.App.Persistencia {
     
-    public class RepositoryEmployer : IRepositoryEmployer {
+    public class RepositoryEmployer {
 
-        /// <summary>
-        /// Referencia al contexto de usuarios
-        /// </summary>
-
-        private readonly AppContext _appContext;
-        /// <summary>
-        /// Metodo constructor utiliza
-        /// Inyeccion de dependencias para indicar el contexto a utilizar
-        /// </summary>
-
-        public RepositoryEmployer(AppContext appContext) {
-            _appContext = appContext;
-        }
-
-        Employer IRepositoryEmployer.AddEmployer(Employer employer) {
-
-            var employerAdd = _appContext.Employers.Add(employer);
-            _appContext.SaveChanges();
-            return employerAdd.Entity;
-
-        }
+       
     }
 
 }
